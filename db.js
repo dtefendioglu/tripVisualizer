98dda10ea48b2c6043f78db360a3d2db805ddb08
dtefendioglu <dtefendioglu@gmail.com>
2017-03-25 16:48:33 -0700

    some cleanup, adding clustering support

--- db.js
+++ db.js
@@ -2,17 +2,20 @@ var bunyan = require('bunyan'),
 	logger = bunyan.createLogger({name: "db"}),
 	mongodb = require('mongodb');
 
-// db
-var init = function(connStr, callback) {
+var globalDB = null;
+var init = function(config, callback) {
+	var connStr = "mongodb://" + config.host + ":" + config.port + "/" + config.name;
 	logger.info("attempting to connect to db: " + connStr);
 	var client = mongodb.MongoClient;
-	var db = client.connect(connStr, function(err, db) {
+	client.connect(connStr, function(err, db) {
 		if(err) {
 			logger.error("error connecting to mongo! "+err);
 			callback({ "code": 1, "error": err });
+			globalDB = null;
 			return;
 		}
 
+		globalDB = db;
 		db.createCollection('messages', function(err, db){});
 		callback({ "code": 0, "error": "" });
 	});
--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 // modules
 var http = require('http'),
 	bunyan = require('bunyan'),
-	logger = bunyan.createLogger({name: "server"});
 	express = require('express'),
+	cluster = require('cluster'),
 	db = require('./db.js');
 
-var initSuccess = true;
+var logger = bunyan.createLogger({name: "server"}),
+	initSuccess = true;
 
 // config
 var config = require('./config.json');
 logger.info("config loaded");
-logger.info(config);
 
 // db init
-var connStr = "mongodb://" + config.db.host + ":" + config.db.port + "/" + config.db.name;
-db.init(connStr, function(ret) {
+db.init(config.db, function(ret) {
 	if(ret.code !== 0)
 		initSuccess = false;
+	else
+		logger.info("db init success!");
 });
 
 // handlers
 var helpHandler = require('./handlers/help.js');
 
 // server routing
-var app = express();
-app.get("/", function(req, res, next) {
-	res.send("blank page");
-});
-
-app.use("/help", helpHandler);
-
-// LISTEN!
 if(initSuccess) {
-	logger.info("listening on %s", config.server.port);
-	app.listen(config.server.port);
+	if(cluster.isMaster) {
+		// TODO deal with getting os cores later
+		cluster.fork();
+	}
+	else {
+		var app = express();
+		app.get("/", function(req, res, next) {
+			res.send("blank page");
+		});
+
+		logger.info("setting up handlers");
+		app.use("/help", helpHandler);
+
+		logger.info("listening on %s", config.server.port);
+		app.listen(config.server.port);
+	}
 }
 else
 	logger.error("there was an init error, exiting");
